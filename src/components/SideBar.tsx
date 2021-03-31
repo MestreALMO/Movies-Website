84192e5e71a7cda588b98a4010bc51b18ae802e4
André Lusegardis <lusegardis@gmail.com>
2021-03-31 12:23:33 -0300

    Done

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,36 @@
-export function SideBar() {
+import { Button } from './Button';
+import '../styles/sidebar.scss';
+
+interface Genre {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps {
+  genres: Genre[],
+  selectedGenreId: number;
+  onClickCategory: (id: number) => void;
+}
+
+export function SideBar({genres, selectedGenreId, onClickCategory}: SideBarProps) {
   // Complete aqui
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onClickCategory(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  );
 }
